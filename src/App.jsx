7045fa3e2dc22e5d1f3b375b93ba7996a2f4c751
Sudhansu Sekhar Behera <118283474+skehargit@users.noreply.github.com>
2024-05-12 18:06:49 +0530

    appcss delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet, Route, Routes } from 'react-router-dom'
-import './App.css'
+
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Layout from './Layout'
